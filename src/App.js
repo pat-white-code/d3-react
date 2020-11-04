e8e4a3c6502cde71e0f12cd0444d0131f47f487c
pat-white-code <pat.k.white@gmail.com>
2020-11-04 15:59:34 -0600

    makes axis component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Axis from './components/Axis';
 
 function App() {
   return (
     <svg width="800" height="400" id="svg">
-      
+      <Axis />
     </svg>
   );
 }
