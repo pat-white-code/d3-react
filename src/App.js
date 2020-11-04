cd2d63aab236fe927d541d7e8b9c07cb16a9a4c1
pat-white-code <pat.k.white@gmail.com>
2020-11-04 15:42:05 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -3,22 +3,9 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <svg width="800" height="400" id="svg">
+      
+    </svg>
   );
 }
 
