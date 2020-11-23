f0694f2073342114510f0e6cabfa4f66d0c33274
pat-white-code <pat.k.white@gmail.com>
2020-11-23 12:06:47 -0600

    makes so scatterplot responds to changes in props

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
+import React, { Component } from 'react';
 import './App.css';
 import './styles.css';
 import Scatterplot from './components/Scatterplot';
-import Scatterplot2 from './components/Scatterplot2';
 import * as d3 from 'd3';;
 
 const data = d3.range(100).map(() => [Math.floor(Math.random()*100), Math.floor(Math.random() * 100)])
 
-function App() {
-  console.log(data);
-  return (
-    <div>
-      <svg width="800" height="800" id="svg">
-        <Scatterplot y={50} x={50} width={300} height={300} data={data} />
-      </svg>
-      <svg width="800" height="800">
-          <Scatterplot2 x={50} y={50} width={300} height={300} data={data} />
-      </svg>
-    </div>
-  );
+
+class App extends Component {
+  state = {
+    width: 300,
+    height: 300,
+    data: d3.range(100).map(() => [Math.floor(Math.random()*100), Math.floor(Math.random() * 100)])
+  }
+  handleClick = () => {
+    this.setState({
+      width: this.state.width * .8,
+      height: this.state.height * .8,
+    })
+  }
+  render() {
+    return (
+      <div>
+        <svg width="800" height="800" id="svg" onClick={this.handleClick}>
+          <Scatterplot y={50} x={50} width={this.state.width} height={this.state.height} data={this.state.data} />
+        </svg>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/Scatterplot.js
+++ src/components/Scatterplot.js
@@ -4,26 +4,37 @@ import Axis2 from './Axis2';
 
 
 class Scatterplot extends Component {
-  xScale = d3
+  state = {
+    xScale: d3
       .scaleLinear()
       .domain([0, 100])
-      .range([0, this.props.width]);
+      .range([0, this.props.width]),
+    yScale: d3
+      .scaleLinear()
+      .domain([0, 100])
+      .range([0, this.props.height])
+  }
 
-  yScale = d3
-      .scaleLinear()
-      .domain([0, 100])
-      .range([this.props.height, 0]);
+  static getDerivedStateFromProps(props, state) {
+    let {xScale, yScale } = state;
+
+    xScale.range([0, props.width])
+    yScale.range([0, props.height])
+
+    return {...state, xScale, yScale}
+  }
 
   render() {
-    const {x, y, data, height} = this.props;
+    const {x, y, data, height} = this.props,
+    {xScale, yScale} = this.state;
 
     return (
       <g transform={`translate(${x}, ${y})`}>
         {data.map(([x,y], idx) => (
-          <circle key={idx} cx={this.xScale(x)} cy={this.yScale(y)} r={3} />
+          <circle key={idx} cx={this.state.xScale(x)} cy={this.state.yScale(y)} r={3} />
         ))}
-        <Axis2 x={0} y={0} type='Left' scale={this.yScale} />
-        <Axis2 x={0} y={height} scale={this.xScale} type="Bottom" />
+        <Axis2 x={0} y={0} type='Left' scale={yScale} />
+        <Axis2 x={0} y={height} scale={xScale} type="Bottom" />
       </g>
     );
   }
