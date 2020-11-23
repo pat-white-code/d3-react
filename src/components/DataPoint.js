565e7c60379ed982bab958cee67defc92e9513df
pat-white-code <pat.k.white@gmail.com>
2020-11-23 12:54:33 -0600

    makes circles their separate styled components

--- src/components/DataPoint.js
+++ src/components/DataPoint.js
@@ -3,12 +3,19 @@ import styled from 'styled-components';
 
 const Circle = styled.circle`
   fill: steelblue;
+  stroke: steelblue;
+  stroke-width: 2px;
+  fill-opacity: 0.5;
+  &:hover{
+    fill: red;
+    stroke: red;
+  }
 `
 
 const DataPoint = props => {
   const {x, y} = props;
   return (
-    <Circle cx={x} cy={y} />
+    <Circle cx={x} cy={y} r={5} />
   )
 };
 
