4d9b34ec86900257eeb149750a09cadc238f95df
pat-white-code <pat.k.white@gmail.com>
2020-11-24 10:19:59 -0600

    refactors app.js with hooks

--- src/components/Axis2.js
+++ src/components/Axis2.js
@@ -1,6 +1,8 @@
 import React, { useRef, useEffect } from "react";
 import * as d3 from "d3";
 import styled from "styled-components";
+import { useD3 } from 'd3blackbox';
+
 
 const Axis = ({x, y, scale, type, label}) => {
   const Text = styled.text`
@@ -9,31 +11,35 @@ const Axis = ({x, y, scale, type, label}) => {
     font-size: 10px;
 `;
 
-  const labelPos = () => {
+  // const labelPos = () => {
 
-    switch (type) {
-      case "Top":
-        return { x: scale.range()[1] + 20, y: 0 };
-      case "Right":
-        return { x: 20, y: 0 };
-      case "Bottom":
-        return { x: scale.range()[1] + 25, y: 25 };
-      case "Left":
-        return { x: -25, y: 0 };
-      default:
-        return { x: -25, y: 0 };
-    }
-  }
-  useEffect(()=>{
-    d3.select(gRef.current).call(d3[`axis${type}`](scale));
+  //   switch (type) {
+  //     case "Top":
+  //       return { x: scale.range()[1] + 20, y: 0 };
+  //     case "Right":
+  //       return { x: 20, y: 0 };
+  //     case "Bottom":
+  //       return { x: scale.range()[1] + 25, y: 25 };
+  //     case "Left":
+  //       return { x: -25, y: 0 };
+  //     default:
+  //       return { x: -25, y: 0 };
+  //   }
+  // }
+
+  // useEffect(()=>{
+  //   d3.select(gRef.current).call(d3[`axis${type}`](scale));
+  // })
+
+  // const gRef = useRef();
+  const gRef = useD3(anchor => {
+    d3.select(anchor).call(d3[`axis${type}`](scale));
   })
-
-  const gRef = useRef();
-
 
   return(
     <g ref={gRef} transform={`translate(${x}, ${y})`}>
-      <Text {...labelPos}>{label}</Text>
+      <Text>{label}</Text>
+      {/* <Text {...labelPos}>{label}</Text> */}
     </g>
   )
 }
--- src/components/Scatterplot.js
+++ src/components/Scatterplot.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import * as d3 from 'd3';
 import Axis2 from './Axis2';
-import AxisClass from './AxisClass';
+
 
 
 class Scatterplot extends Component {
