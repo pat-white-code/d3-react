dc33cb34ee87a49eff2135be96cf5ef33cc55783
pat-white-code <pat.k.white@gmail.com>
2020-11-27 13:50:00 -0600

    adds DataHandling

--- src/App.js
+++ src/App.js
@@ -1,19 +1,41 @@
 import React, { useEffect, useState } from 'react';
+
+import * as d3 from "d3"
+import _ from "lodash"
+
 import './App.css';
 import './styles.css';
 import "bootstrap/dist/css/bootstrap.css"
+
 import Preloader from './components/PreLoader';
+import { loadAllData } from './DataHandling';
 
 const App = () => {
-  const [data, setData] = useState([1,2,3]);
+  const [techSalaries, setTechSalaries] = useState([]);
+  const [ medianSalaries, setMedianSalaries] = useState([]);
+  const [ countyNames, setCountyNames] = useState([]);
+
+  const loadData = async() => {
+    const data = await loadAllData();
+    console.log(data);
+    const {techSalaries, medianSalaries, countyNames} = data;
+      
+      setTechSalaries(techSalaries)
+      setMedianSalaries(medianSalaries)
+      setCountyNames(countyNames)
+  }
+
+  useEffect(()=>{
+    loadData();
+  }, [])
 
     return (
       <div>
-        {data.length > 1 ? (
+        {techSalaries.length < 1 ? (
           <Preloader />
         ) : (
           <div className='app'>
-            <h1> this is your app</h1>
+            <h1>Loaded {techSalaries.length} salaries</h1>
           </div>
         )}
       </div>
